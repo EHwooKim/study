9050072e04b62058843f99f68efaefb2256c43e8
EHwooKim <ehwoo0707@naver.com>
2020-01-08 23:18:09 +0900

    20200108 | JS 문자열메소드

---
+++ JavaScript/part3/073.js
@@ -0,0 +1,6 @@
+// concat  - 문자열 합치기
+const str1 = 'Good afternoon'
+const str2 = ', Good evening'
+const str3 = ', and Good night!'
+const str4 = ' - Truman SHow, 1998'
+console.log(str1.concat(str2, str3, str4))
