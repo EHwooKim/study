9c128adad35745401584ad9dce232a570a809c34
김태우 <ehwoo0707@naver.com>
2020-01-10 17:50:02 +0900

    20200110 | JS 객체 메소드

---
+++ JavaScript/part3/101.js
@@ -0,0 +1,10 @@
+// keys - 객체의 키만 배열로 반환
+const obj = {
+    movie: 'Sunny',
+    music: 'Like Sugar',
+    style: 'Retro',
+    price: Infinity
+}
+
+const arr = Object.keys(obj)
+console.log(arr)
---
+++ JavaScript/part3/103.js
@@ -0,0 +1,10 @@
+// entries - 객체를 배열로 변환.
+const obj = {
+    movie: 'Sunny',
+    music: 'Like Sugar',
+    style: 'Retro',
+    price: Infinity
+}
+
+const modifiedObj = Object.entries(obj)
+console.log(modifiedObj)
