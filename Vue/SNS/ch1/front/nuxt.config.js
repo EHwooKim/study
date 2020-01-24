be06f13180a1fef2878369d82a8a4b2c639e71a0
김태우 <ehwoo0707@naver.com>
2020-01-24 22:41:12 +0900

    20200124 | vuetify 적용

--- Vue/SNS/ch1/front/nuxt.config.js
+++ Vue/SNS/ch1/front/nuxt.config.js
@@ -3,5 +3,15 @@
 module.exports = {
     head: {
         title: 'Nodebird'
+    },
+    modules: [              //이와같이 적어주면 외부 라이브러리 연결이 되고
+        '@nuxtjs/axios'
+    ],
+    devModules: [
+        '@nuxtjs/vuetify',  // 공식문서에 따라 vuetify는 이곳에 추가.
+    ],
+    plugins: [],
+    vuetify : {             // 연결 후 연결한 라이브러리에 대한 설정을 또 해줄 수 있다.
+
     }
 }
