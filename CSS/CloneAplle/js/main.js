6acf81172306e392569d94662d55364f210fb496
EHwooKim <ehwoo0707@naver.com>
2020-06-01 21:55:14 +0900

    스크롤 높이 세팅

---
+++ CSS/CloneAplle/js/main.js
@@ -0,0 +1,47 @@
+(() => {
+  const sceneInfo = [ // 각 씬에 대한 정보를 담을 배열
+    { // 0
+      type: 'sticky',
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight를 세팅하겠다. (기기마다 높이가 다르기 때문에 고정값을 주면 애니메이션이 원하는대로 작동 안할수있기때문에)
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-0')
+      }
+    },
+    { // 1
+      type: 'normal',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-1')
+      }
+    },
+    { // 2
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-2')
+      }
+    },
+    { // 3
+      type: 'sticky',
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector('#scroll-section-3')
+      }
+    },
+  ]
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight
+      sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
+    }
+    console.log(sceneInfo)
+  }
+
+  window.addEventListener('resize', setLayout) // 브라우저 크기 변할 떄마다 높이 재설정을 위해
+  setLayout()
+})()
