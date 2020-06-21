90cbb9f0b41005f41292f91a4788623fcd73e93a
EHwooKim <ehwoo0707@naver.com>
2020-06-21 13:10:28 +0900

    React 리액트 반복문(key)

--- React/webGame/3_숫자야구/NumberBaseball.jsx
+++ React/webGame/3_숫자야구/NumberBaseball.jsx
@@ -23,14 +23,21 @@ class NumberBaseball extends React.Component {
     return (
       <>
         <h1>{this.state.result}</h1>
-        <form onSUbmit={this.onSubmitForm}>
+        <form onSubmit={this.onSubmitForm}>
           <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
         </form>
         <div>시도: {this.state.tries.length}</div>
         <ul>
-          {['like1', 'like2', 'like3', 'like4'].map((v) => {
+          {[
+            {fruit: '사과', taste:'맛있다'},
+            {fruit: '포도', taste:'맛있다'},
+            {fruit: '바나나', taste:'맛없다'},
+            {fruit: '귤', taste:'시다'}
+          ].map((v) => {
             return (
-              <li>{v}</li>
+              <li key={v.fruit}><b>{v.fruit}</b> - {v.taste}</li> // Vue와 마찬가지로 반복문에는 key가 필요하다.
+              // map((v, i) => {...})한 다음에 i값이 고유하니 key에 i를 넣는 행동은 성능최적화에 안좋으니 하지말자, 차라리 v.fruit + i 와 같이 쓰자(하지만 i를 안쓰는게 제일 좋다)
+              // 요소 삭제 없이 추가만 되는 배열의 경우 i를 써도 되긴하다.
             )
           })}
           <li />
