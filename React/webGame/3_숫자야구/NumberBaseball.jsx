bd4310b838145c177ef3eeb9aab9a607f6862999
EHwooKim <ehwoo0707@naver.com>
2020-06-22 15:58:10 +0900

    React 코드수정

--- React/webGame/3_숫자야구/NumberBaseball.jsx
+++ React/webGame/3_숫자야구/NumberBaseball.jsx
@@ -4,6 +4,11 @@ const Try  = require('./Try')
 // import React, { Component } from 'react'
 // import Try from './Try'
 
+/* 
+  class 안에 넣어도 상관은 없다.
+  그런데 this를 안쓰면 class 밖에 빼도 상관없기 때문에 밖에 써준다.
+  또 이 함수를 다른 파일에서 재사용할 수도 있는데 class 안에 쓰면 재사용이 번거롭기때문에서도 class 밖에 쓴다.
+*/
 function getNumbers() { // 숫자 네 개를 중복없이 랜덤하게 뽑는 함수
   const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   const array = []
@@ -41,18 +46,19 @@ class NumberBaseball extends Component {
   // }  
 
   onSubmitForm = (e) => {
+    const { result, value, answer, tries } = this.state
     e.preventDefault()
-    if (this.state.value.length !== 4) {
+    if (value.length !== 4) {
       alert('숫자 4개를 입력해주세요')
       this.setState({
         value: ''
       })
       return
     }
-    if ( this.state.value === this.state.answer.join('')) { // 답일 경우
+    if ( value === answer.join('')) { // 답일 경우
       this.setState({
         result: '홈런!',
-        tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }], // 참조를 바뀌게 하여 리액트가 변화를 감지할 수 있게 해야한다. (push금지)
+        tries: [...tries, { try: value, result: '홈런!' }], // 참조를 바뀌게 하여 리액트가 변화를 감지할 수 있게 해야한다. (push금지)
       })
       alert('게임을 다시 시작합니다!')
       this.setState({
@@ -61,12 +67,12 @@ class NumberBaseball extends Component {
         tries: [],
       })
     } else { // 답이 아닐경우
-      const answerArray = this.state.value.split('').map(v => parseInt(v))
+      const answerArray = value.split('').map(v => parseInt(v))
       let strike = 0
       let ball = 0
-      if (this.state.tries.length >= 9) { // 10번 이상 틀려 실패했을 때
+      if (tries.length >= 9) { // 10번 이상 틀려 실패했을 때
         this.setState({
-          result: `10번 넘게 틀려서 실패! ${this.state.answer.join(',')} 였습니다.`
+          result: `10번 넘게 틀려서 실패! ${answer.join(',')} 였습니다.`
         })
         alert('게임을 다시 시작합니다!')
         this.setState({
@@ -76,16 +82,16 @@ class NumberBaseball extends Component {
         })
       } else {
         for (let i = 0; i < 4; i +=1 ) {
-          if (answerArray[i] === this.state.answer[i]) {
+          if (answerArray[i] === answer[i]) {
             console.log('스트라이크')
             strike += 1
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             console.log('볼')
             ball += 1
           }
         }
         this.setState({
-          tries: [...this.state.tries, { try: this.state.value, result: `${strike}스트라이크, ${ball}볼입니다.`}],
+          tries: [...tries, { try: value, result: `${strike}스트라이크, ${ball}볼입니다.`}],
           value: '',
         })
       }
@@ -98,16 +104,17 @@ class NumberBaseball extends Component {
   }
 
   render() {
+    const { result, value, answer, tries } = this.state
     return (
       <>
       {/* jsx 주석 */}
-        <h1>{this.state.result}</h1>
+        <h1>{result}</h1>
         <form onSubmit={this.onSubmitForm}>
-          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
+          <input maxLength={4} value={value} onChange={this.onChangeInput} />
         </form>
-        <div>시도: {this.state.tries.length}</div>
+        <div>시도: {tries.length}</div>
         <ol>
-          {this.state.tries.map((v, i) => {
+          {tries.map((v, i) => {
             return (
               <Try key={`${i + 1}차 시도 : `} tryInfo={v} />
             )
