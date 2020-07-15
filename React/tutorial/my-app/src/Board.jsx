baad2823b18eb6652c768a5fad463c743d3e5f81
EHwooKim <ehwoo0707@naver.com>
2020-07-15 17:03:36 +0900

    React - tutorial 게임 완성

--- React/tutorial/my-app/src/Board.jsx
+++ React/tutorial/my-app/src/Board.jsx
@@ -2,12 +2,65 @@ import React from 'react'
 import Square from './Square'
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+
+  handleClick = (i) => () => {
+    const squares = [...this.state.squares]
+    if (this.calculateWinner(squares) || squares[i]) {
+      return
+    }
+
+    this.setState((prevState) => {
+      let array = [...prevState.squares]
+      array[i] = this.state.xIsNext ? 'X' : 'O'
+      return {
+        squares: array,
+        xIsNext: !prevState.xIsNext
+      }
+    })
+  }
+
+  renderSquare = (i) => {
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={this.handleClick(i)}
+      />
+    )
+  }
+  calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a]
+      }
+    }
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = this.calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `Winner ${winner}`
+    } else {
+      status = `Next player : ${ this.state.xIsNext ? 'X' : 'O' }`
+    }
 
     return (
       <div>
