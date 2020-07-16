3b507c712889232426edfed0da29a093d0f8617b
EHwooKim <ehwoo0707@naver.com>
2020-07-16 18:09:16 +0900

    React - tutorial step 이동 추가

--- React/tutorial/my-app/src/Square.jsx
+++ React/tutorial/my-app/src/Square.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 // state 필요 없는 컴포넌트의 경우 함수 컴포넌트로 작성하면 좋다
-// function Square(props) {
-//   return (
-//     <button className="square" onClick={props.onClick}>
-//       {props.value}
-//     </button>
-//   )
-// }
-
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={this.props.onClick}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
+
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={this.props.onClick}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
 
 export default Square
