74789182c46239574a1bfa56f8783e343f5e121c
EHwooKim <ehwoo0707@naver.com>
2020-07-25 21:22:43 +0900

    React - Router tutorial

--- React/tutorial/README.md
+++ React/tutorial/README.md
@@ -4,3 +4,7 @@
 
 [자습서](https://ko.reactjs.org/tutorial/tutorial.html)
 
+# ReactRouter
+
+[공식문서](https://reactrouter.com/web/guides/quick-start)
+
--- React/tutorial/my-app/src/App.jsx
+++ React/tutorial/my-app/src/App.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 // import Game from './ClassComponents/Game'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Game from './HooksComponents/Game'
+import Home from './HooksComponents/Home'
+
 
 class App extends React.Component {
 
   render() {
     return (
-      <Game />
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/game">Game</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
     )
   }
 }
