802cff6c29395c0909f82f617aa2955e90fbbfc7
EHwooKim <ehwoo0707@naver.com>
2020-07-27 22:57:48 +0900

    React - Link와 브라우저 라우터

--- React/webGame/9_리액트라우터/Games.jsx
+++ React/webGame/9_리액트라우터/Games.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Link } from 'react-router-dom'
 import NumberBaseball from './NumberBaseball/NumberBaseball'
 import RSP from './RSP/RSP'
 import Lotto from './Lotto/Lotto'
@@ -8,11 +8,19 @@ import Lotto from './Lotto/Lotto'
 const Games = () => {
   return (
     <BrowserRouter>
-    <div>
-      <Route path="/number-baseball" component={NumberBaseball}></Route>
-      <Route path="/rock-scissors-paper" component={RSP}></Route>
-      <Route path="/lotto-generator" component={Lotto}></Route>
-    </div>
+      <div>
+        공통인 부분
+        <Link to="/number-baseball">숫자야구</Link>
+        &nbsp;
+        <Link to="/rock-scissors-paper">가위바위보</Link>
+        &nbsp;
+        <Link to="/lotto-generator">로또추첨기</Link>
+      </div>
+      <div>
+        <Route path="/number-baseball" component={NumberBaseball}></Route>
+        <Route path="/rock-scissors-paper" component={RSP}></Route>
+        <Route path="/lotto-generator" component={Lotto}></Route>
+      </div>
     </BrowserRouter>
   )
 }
