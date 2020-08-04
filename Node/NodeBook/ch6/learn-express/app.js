16281bc966edc98fb218301da18e7c30eb275d72
EHwooKim <ehwoo0707@naver.com>
2020-08-04 14:24:04 +0900

    Fix typo

--- Node/NodeBook/ch6/learn-express/app.js
+++ Node/NodeBook/ch6/learn-express/app.js
@@ -28,8 +28,8 @@ app.use(function(req, res, next) {
 
 app.use(logger('dev'));   // app.use로 시작하는 코드들은 미들웨어를 연결하는 부분으로  미들웨어는 익스프레스의 핵심이라고 할 수 있습니다.
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());  // app.use 메서드의 인자로 들어가있는 함수가 미들웨어입니다. 
-app.use(express.urlencoded({ extended: false })); // 내부적으로 next()를 호출하여 다음 미들웨어로 넘어가게 됩니다.
+app.use(express.json());  // app.use 메서드의 인자로 들어가있는 함수가 미들웨어입니다.  // express 4.16.0 부터 body-parser의 일부기능이 내장되어 이처럼 사용했다.
+app.use(express.urlencoded({ extended: false })); // 내부적으로 next()를 호출하여 다음 미들웨어로 넘어가게 됩니다. // 버퍼 데이터, 텍스트 데이터의 경우 내장기능으로 해석이 안되니 body-parser를 설치하여 사용하자.
 app.use(cookieParser('secret code'));
 app.use(session({   // express-session 은 cookie-parser보다 뒤에 놓는 것이 안전합니다. (1.5버전 이전에는 express-session 내부적으로 cookie-parser를 사용했기떄문에)
   resave: false,    // 요청이 왔을 때 수정사항이 생기지 않더라도 세션을 다시 저장할지에 대한 설정
