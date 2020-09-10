3d33ee0648c96bfed165f8b37d46e8c7163ce3ff
EHwooKim <ehwoo0707@naver.com>
2020-09-10 13:46:26 +0900

    ReactShoppingMall - Get products Info

--- React/ShoppingMall/server/routes/product.js
+++ React/ShoppingMall/server/routes/product.js
@@ -40,4 +40,17 @@ router.post('/', (req, res) => {
   })
 })
 
+router.post('/products', (req, res) => {
+  // product collection에 들어있는 모든 상품 정보를 가져오기
+  // find() : 모든 정보 검색 인자로 조건을 넣을 수도 있다.
+  // populate : writer에 현재 id만 들어있는데 writer의 모든 정보를 가져오기 위해 사용
+  Product.find()
+    .populate('writer')
+    .exec((err, productInfo) => {
+      if (err) return res.status(400).json({ success: false, err })
+      return res.status(200).json({ success: true, productInfo })
+    })
+  
+})
+
 module.exports = router;
