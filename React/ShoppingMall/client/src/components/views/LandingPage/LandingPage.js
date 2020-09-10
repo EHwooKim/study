e58bf2d6a650731a54ea416d705d60c8d88162c5
EHwooKim <ehwoo0707@naver.com>
2020-09-10 14:02:31 +0900

    ReactShoppingMall - Add product card at landingPage

--- React/ShoppingMall/client/src/components/views/LandingPage/LandingPage.js
+++ React/ShoppingMall/client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from 'axios'
-
+import { Icon, Col, Card, Row } from 'antd'
+import Meta from 'antd/lib/card/Meta'
 
 
 function LandingPage() {
+  const [products, setProducts] = useState([])
 
   useEffect(() => {
     let body = {}
@@ -12,17 +14,46 @@ function LandingPage() {
     axios.post('/api/product/products')
       .then(res => {
         if (res.data.success) {
-          console.log(res.data)
+          setProducts(res.data.productInfo)
         } else {
           alert('상품들을 가져오는데 실패했습니다.')
         }
       })
+  }, [])
+
+
+  const renderCards = products.map((product, index) => {
+    return (
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card
+          cover={<img style={{ width: '100%', maxHeight: '150px' }} src={`http://localhost:5000/${product.images[0]}`} />}
+        >
+          <Meta
+            title={product.title}
+            description={`$${product.price}`}
+          />
+        </Card>
+      </Col>
+    )
   })
 
-
   return (
-    <div>
-      Landing Page
+    <div style={{ width: '75%', margin: '3rem auto' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h2>Let's Travel Anywhere <Icon type="rocket"></Icon></h2>
+      </div>
+
+      {/* Filter */}
+      {/* Search */}
+      {/* Cards */}
+      <Row gutter={[16, 16]}>
+        {renderCards}
+      </Row>
+
+
+      <div style={{ justifyContent: 'center' }}>
+        <button>더보기</button>
+      </div>
     </div>
   ) 
 }
