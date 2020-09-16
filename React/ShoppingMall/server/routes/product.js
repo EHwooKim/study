9f64b69cdc2716d3b30a2531e4d3d318c2306297
EHwooKim <ehwoo0707@naver.com>
2020-09-16 16:21:31 +0900

    ReactShoppingMall - Add search

--- React/ShoppingMall/server/routes/product.js
+++ React/ShoppingMall/server/routes/product.js
@@ -44,6 +44,7 @@ router.post('/products', (req, res) => {
   // product collection에 들어있는 모든 상품 정보를 가져오기
   let limit = req.body.limit ? parseInt(req.body.limit) : 10
   let skip = req.body.skip ? parseInt(req.body.skip) : 0
+  let term = req.body.searchTerm
 
   // filter
   let findArgs = {}
@@ -61,9 +62,9 @@ router.post('/products', (req, res) => {
     }
   }
 
-  // find() : 모든 정보 검색 인자로 조건을 넣을 수도 있다.
-  // populate : writer에 현재 id만 들어있는데 writer의 모든 정보를 가져오기 위해 사용
-  Product.find(findArgs)
+  if (term) {
+    Product.find(findArgs)
+    .find({ $text: { $search: term } })
     .populate('writer')
     .skip(skip)
     .limit(limit)
@@ -75,7 +76,22 @@ router.post('/products', (req, res) => {
         postSize: productInfo.length
       })
     })
-  
+  } else {
+    // find() : 모든 정보 검색 인자로 조건을 넣을 수도 있다.
+    // populate : writer에 현재 id만 들어있는데 writer의 모든 정보를 가져오기 위해 사용
+    Product.find(findArgs)
+    .populate('writer')
+    .skip(skip)
+    .limit(limit)
+    .exec((err, productInfo) => {
+      if (err) return res.status(400).json({ success: false, err })
+      return res.status(200).json({
+        success: true, 
+        productInfo, 
+        postSize: productInfo.length
+      })
+    })
+  }
 })
 
 module.exports = router;
