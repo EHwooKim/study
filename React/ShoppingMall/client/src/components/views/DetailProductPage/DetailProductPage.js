682f7a955d92d123b818e16dea2cd7786325b9d0
EHwooKim <ehwoo0707@naver.com>
2020-09-18 18:17:55 +0900

    ReactShoppingMall - Add product image component

--- React/ShoppingMall/client/src/components/views/DetailProductPage/DetailProductPage.js
+++ React/ShoppingMall/client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import ProductImage from './Sections/ProductImage'
+import ProductInfo from './Sections/ProductInfo'
+import { Row, Col } from 'antd'
 
 function DetailProductPage(props) {
 
   const productId = props.match.params.productId
+  const [product, setProduct] = useState({})
 
   useEffect(() => {
     axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
       .then(res => {
         console.log(res.data)
         if (res.data.success) {
-          
+          setProduct(res.data.product[0])
         } else {
           alert('상세 정보 가져오기를 실패했습니다.')
         }
@@ -18,8 +22,19 @@ function DetailProductPage(props) {
   }, [])
 
   return (
-    <div>
-      DetailProductPage
+    <div style={{ width: '100%', padding: '3rem 4rem' }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h1>{product.title}</h1>
+      </div>
+      <br />
+      <Row gutter={[16, 16]} >
+        <Col lg={12} sm={24}>
+          <ProductImage detail={product}/>          
+        </Col>
+        <Col lg={12} sm={24}>
+          <ProductInfo /> 
+        </Col>
+      </Row>
     </div>
     
   )
---
+++ React/ShoppingMall/client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import ImageGallery from 'react-image-gallery'
+
+function ProductImage(props) {
+
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    if (props.detail.images && props.detail.images.length > 0) {
+      let newImages = []
+
+      props.detail.images.map(item => {
+        newImages.push({
+          original: `http://localhost:5000/${item}`,
+          thumbnail: `http://localhost:5000/${item}`
+        })
+      })
+      setImages(newImages)
+    }
+  }, [props.detail])
+
+
+  return (
+    <div>
+      <ImageGallery items={images} />
+    </div>
+  )
+}
+
+export default ProductImage
