0dcf7c2d10877ca42f5915394f6c04592b83e378
EHwooKim <ehwoo0707@naver.com>
2020-10-16 13:01:18 +0900

    실습 - Prettier

---
+++ WebPack_Babel/ESLint/04(실습 - Prettier)/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        // TODO: 프리티어 설정을 추가하세요.
+        "plugin:prettier/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
