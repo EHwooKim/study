e4c14987f08fd08e1181cdcce8bff4d0f9211121
EHwooKim <ehwoo0707@naver.com>
2020-11-18 21:08:54 +0900

    React - Add animation

--- React/기타/capture/src/components/AboutSection.js
+++ React/기타/capture/src/components/AboutSection.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import home1 from '../img/home1.png'
+import Wave from './Wave'
 import { About, Description, Image, Hide } from '../styles'
 
 import { motion } from 'framer-motion'
+import { titleAnim, fade, photoAnim } from '../animation'
+import styled from 'styled-components'
 
 
 function Aboutsection() {
@@ -11,33 +14,45 @@ function Aboutsection() {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>
+            <motion.h2
+              variants={titleAnim}
+            >
               We work to make
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2
+              variants={titleAnim}
+            >
               Your<span> dreams</span> <span>come</span>
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2
+              variants={titleAnim}
+            >
               true.
             </motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p
+          variants={fade}
+        >
           Contact us for any photography or videography ideas that you have. We have professionals with amazing skills
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button
+          variants={fade}
+        >
+          Contact Us
+        </motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="a guy with camera" />
+        <motion.img variants={photoAnim} src={home1} alt="a guy with camera" />
       </Image>
+      <Wave />
     </About>
   )
 }
-
 
 
 export default Aboutsection
