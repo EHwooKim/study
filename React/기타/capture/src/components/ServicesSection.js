fb1ab9efc2b497e167f76b568b33efb9cb74071c
EHwooKim <ehwoo0707@naver.com>
2020-11-20 14:09:33 +0900

    React - Add scroll animation

--- React/기타/capture/src/components/ServicesSection.js
+++ React/기타/capture/src/components/ServicesSection.js
@@ -8,11 +8,15 @@ import home2 from '../img/home2.png'
 
 import styled from 'styled-components'
 import { About, Description, Image } from '../styles'
+import { fade } from '../animation'
+import { useScroll } from './useScroll'
 
 function ServicesSection() {
+  const [element, controls] = useScroll()
+
   return (
-    <Services>
-      <Description>
+    <Services variants={fade} animate={controls} initial="hidden" ref={element}>
+      <Description >
         <h2>High <span>quality </span><span>services</span></h2>
         <Cards>
           <Card>
