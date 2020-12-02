a8a438332fccb57824592fd218ce7500055f1065
EHwooKim <ehwoo0707@naver.com>
2020-12-02 13:59:16 +0900

    React - Resize img

--- React/기타/ignite/src/components/Game.js
+++ React/기타/ignite/src/components/Game.js
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion'
 import { useDispatch } from 'react-redux'
 import { loadDetail } from '../actions/detailAction'
 import { Link } from 'react-router-dom'
+import { smallImage } from '../util'
 
 function Game({ name, released, id, image }) {
   const dispatch = useDispatch()
@@ -17,7 +18,7 @@ function Game({ name, released, id, image }) {
       <Link to={`/game/${id}`}>
         <h3>{name}</h3>
         <p>{released}</p>
-        <img src={image} alt={name} />
+        <img src={smallImage(image, 640)} alt={name} />
       </Link>
     </StyledGame>
   )
