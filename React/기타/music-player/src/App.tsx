c6e2ef451b2a94d5c3439bc625bd57b187fde23d
EHwooKim <ehwoo0707@naver.com>
2020-12-07 10:57:51 +0900

    React - Add song, player components and styles

--- React/기타/music-player/src/App.tsx
+++ React/기타/music-player/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './styles/app.scss'
+
+import Song from './components/Song'
+import Player from './components/Player'
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Music player!</h1>
+      <Song />
+      <Player />
     </div>
   )
 }
