066eb5b15a0c9879938a3cd4c1fc3a1e0e840b14
EHwooKim <ehwoo0707@naver.com>
2020-12-08 14:30:58 +0900

    React.ts - Update timer

--- React/기타/music-player/src/components/Player.tsx
+++ React/기타/music-player/src/components/Player.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, SetStateAction, Dispatch  } from 'react'
+import React, { useState, useRef, SetStateAction, Dispatch, useCallback  } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,34 +9,68 @@ type Props = {
   setIsPlaying: Dispatch<SetStateAction<boolean>>,
   currentSong: SongDataType
 }
+type SongInfoType = {
+  currentTime: number,
+  duration: number
+}
+
 
 const Player:React.FC<Props> = ({ isPlaying, setIsPlaying, currentSong: { audio }}) => {
+  const [songInfo, setSongInfo] = useState<SongInfoType>({ 
+    currentTime: 0,
+    duration: 0,
+  })
   const audioRef = useRef<HTMLAudioElement>(null)
 
-  const playSongHandler = () => {
+
+  const playSongHandler = useCallback(() => {
     if (isPlaying) {
       audioRef.current?.pause()
     } else { 
       audioRef.current?.play()
     }
     setIsPlaying(!isPlaying)
+  }, [isPlaying])
+  
+  const timeUpdateHandler = (e: React.SyntheticEvent<HTMLAudioElement, Event>) => {
+    const current = (e.target as HTMLAudioElement).currentTime
+    const duration = (e.target as HTMLAudioElement).duration
+    setSongInfo({
+      ...songInfo, 
+      currentTime: current,
+      duration: duration
+    })
   }
 
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start Time</p>
+        <p>{getTime(songInfo.currentTime)}</p>
         <input type="range" />
-        <p>End Time</p>
+        <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
         <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
         <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
       </div>
-      <audio ref={audioRef} src={audio}></audio>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef} 
+        src={audio}
+      ></audio>
     </div>
   )
 }
+
+
+const zeroFill = (num:number): string => num < 10 ? `0${num}` : `${num}`
+const getTime = (time: number): string => {
+  return (
+    `${zeroFill(Math.floor(time / 60))}:${zeroFill(Math.floor(time % 60))}`
+  )
+}
+
 
 export default Player
--- React/기타/music-player/src/components/Song.tsx
+++ React/기타/music-player/src/components/Song.tsx
@@ -9,7 +9,7 @@ type Props = {
 const Song:React.FC<Props> = ({ currentSong: { cover, name, artist } }) => {
   return (
     <div className="song-container">
-      <img src={cover} />
+      <img src={cover} alt={name}/>
       <h2>{name}</h2>
       <h3>{artist}</h3>
     </div>
